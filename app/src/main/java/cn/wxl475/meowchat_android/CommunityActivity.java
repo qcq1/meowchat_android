2e80feb7e4c58658fb8fc58c056444cc7d9da437
wxl <1715142548@qq.com>
2023-09-22 22:49:36 +0800

    feat: 社区界面UI部分

---
+++ app/src/main/java/cn/wxl475/meowchat_android/CommunityActivity.java
@@ -0,0 +1,14 @@
+package cn.wxl475.meowchat_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CommunityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+    }
+}
