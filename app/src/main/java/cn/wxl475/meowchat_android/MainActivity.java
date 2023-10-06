a2fb489c11a206bb65088b469df7c4c713251fda
wxl <1715142548@qq.com>
2023-10-06 13:47:25 +0800

    feat: 取消标题栏

--- app/src/main/java/cn/wxl475/meowchat_android/MainActivity.java
+++ app/src/main/java/cn/wxl475/meowchat_android/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         //发布按钮跳转
         @SuppressLint({"MissingInflatedId", "LocalSuppress"}) View nav_release = findViewById(R.id.navigation_release);
-        nav_release.setOnClickListener(v->{
+        nav_release.setOnClickListener(v -> {
             releaseOnClick();
         });
 
@@ -32,18 +32,18 @@ public class MainActivity extends AppCompatActivity {
         // 配置navigation与底部菜单之间的联系
         // 底部菜单的样式里面的item里面的ID与navigation布局里面指定的ID必须相同，否则会出现绑定失败的情况
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_community,R.id.navigation_collection,R.id.navigation_world,R.id.navigation_user)
+                R.id.navigation_community, R.id.navigation_collection, R.id.navigation_world, R.id.navigation_user)
                 .build();
         // 建立fragment容器的控制器，这个容器就是页面的上的fragment容器
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
         // 启动
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+      //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);//因为取消了app上方的标题栏，所以弃用此行代码，否则无法进入app
         NavigationUI.setupWithNavController(navView, navController);
 
     }
 
-    public void releaseOnClick(){
+    public void releaseOnClick() {
         startActivity(new Intent(MainActivity.this, ReleaseActivity.class));
     }
 }
