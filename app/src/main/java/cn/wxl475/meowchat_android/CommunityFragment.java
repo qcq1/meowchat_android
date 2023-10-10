b17a39602553f317a0c4ac63b7b7c0861a9bc5d6
wxl <1715142548@qq.com>
2023-10-10 14:52:28 +0800

    feat: 回溯

--- app/src/main/java/cn/wxl475/meowchat_android/CommunityFragment.java
+++ app/src/main/java/cn/wxl475/meowchat_android/CommunityFragment.java
@@ -3,10 +3,8 @@ package cn.wxl475.meowchat_android;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -14,11 +12,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.google.gson.Gson;
 
@@ -26,7 +25,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import cn.wxl475.meowchat_android.myClass.my_imageView;
@@ -39,11 +37,12 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class CommunityFragment extends Fragment {
+public class CommunityFragment extends Fragment{
     private View root;
     ViewFlipper flipper;
     private SharedPreferences.Editor editor_CommunityFragment;
     private SharedPreferences getter_CommunityFragment;
+
     @SuppressLint("HandlerLeak")
     private Handler handler=new Handler(){
         @Override
@@ -130,11 +129,13 @@ public class CommunityFragment extends Fragment {
             }
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                assert response.body() != null;
-                news_result newsResult = new Gson().fromJson(response.body().string(), news_result.class);
-                Message msg=new Message();
-                msg.obj=newsResult;
-                handler.sendMessage(msg);
+                if(response.isSuccessful()){
+                    assert response.body() != null;
+                    news_result newsResult = new Gson().fromJson(response.body().string(), news_result.class);
+                    Message msg=new Message();
+                    msg.obj=newsResult;
+                    handler.sendMessage(msg);
+                }
             }
         });
     }
