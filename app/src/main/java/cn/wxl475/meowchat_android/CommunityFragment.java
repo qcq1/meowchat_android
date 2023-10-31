5e9807c5b9fc68b8ef0a821e8443373001357e56
wxl <1715142548@qq.com>
2023-10-31 17:16:35 +0800

    feat: 优化import

--- app/src/main/java/cn/wxl475/meowchat_android/CommunityFragment.java
+++ app/src/main/java/cn/wxl475/meowchat_android/CommunityFragment.java
@@ -8,7 +8,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,8 +29,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import cn.wxl475.meowchat_android.myClass.my_moment_RecyclerViewAdapter;
 import cn.wxl475.meowchat_android.myClass.my_imageView;
+import cn.wxl475.meowchat_android.myClass.my_moment_RecyclerViewAdapter;
 import cn.wxl475.meowchat_android.pojo.Moment;
 import cn.wxl475.meowchat_android.pojo.MomentAllDetail;
 import cn.wxl475.meowchat_android.pojo.News;
@@ -46,7 +45,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class CommunityFragment extends Fragment{
+public class CommunityFragment extends Fragment {
     private View root;
     private ViewFlipper flipper;
     private RecyclerView waterfall;
@@ -56,10 +55,10 @@ public class CommunityFragment extends Fragment{
     private SharedPreferences getter_CommunityFragment;
     protected WeakReference<View> mRootView;
     @SuppressLint("HandlerLeak")
-    private Handler handler = new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if(msg.what==1){
+            if (msg.what == 1) {
                 news_result newsResult = (news_result) msg.obj;
                 List<News> news = newsResult.getNews();
                 for (int i = 0; i < news.size(); i++) {
@@ -76,9 +75,8 @@ public class CommunityFragment extends Fragment{
                     });
                     flipper.addView(myImageView);
                 }
-            }
-            else if(msg.what==2){
-                MomentAllDetail momentAllDetail= (MomentAllDetail) msg.obj;
+            } else if (msg.what == 2) {
+                MomentAllDetail momentAllDetail = (MomentAllDetail) msg.obj;
 
                 //新建一个RecyclerView的适配器，并传入数据
                 myMomentRecyclerViewAdapter = new my_moment_RecyclerViewAdapter(requireContext(), momentAllDetail);
@@ -92,16 +90,18 @@ public class CommunityFragment extends Fragment{
                 waterfall.setLayoutManager(staggeredGridLayoutManager);
             }
         }
+
         //实现我们的监听接口里的方法，在这里获得数据，对数据进行操作
         public void onItemClick(View view, Moment moment) {
             //创建一个intent，指明跳转目标类
             Intent intent = new Intent(getActivity(), MomentDetailActivity.class);
             //拿到数据传给intent
-            intent.putExtra("Moment", new Gson().toJson(moment,Moment.class));
+            intent.putExtra("Moment", new Gson().toJson(moment, Moment.class));
             //启动Activity
             startActivity(intent);
         }
     };
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //在用fragmenttabhost做页面切换的时候，发现只要一来回切换fragment，fragment页面就会重新初始化，也就是执行onCreateView()方法，导致每次Fragment的布局都重绘，无法保持Fragment原有状态
@@ -109,20 +109,20 @@ public class CommunityFragment extends Fragment{
         //在Fragment onCreateView方法中缓存View
         // detach/attach can lead to view recreate frequently
         if (mRootView == null || mRootView.get() == null) {
-            root=inflater.inflate(R.layout.fragment_community,container,false);
+            root = inflater.inflate(R.layout.fragment_community, container, false);
             mRootView = new WeakReference<View>(root);
             //初始化内容
             flipper = root.findViewById(R.id.flipper);
             waterfall = root.findViewById(R.id.waterfall);
-            page=0;
+            page = 0;
 
             getter_CommunityFragment = requireContext().getSharedPreferences("CommunityFragment", MODE_PRIVATE);
             editor_CommunityFragment = getter_CommunityFragment.edit();
 
             editor_CommunityFragment.putString("获取轮播图", "https://meowchat.xhpolaris.com/notice/get_news");
-            editor_CommunityFragment.putString("获取一些动态预览","https://meowchat.xhpolaris.com/moment/get_moment_previews");
-            editor_CommunityFragment.putString("获取点赞数量","https://meowchat.xhpolaris.com/like/get_count");
-            editor_CommunityFragment.putString("获取评论","https://meowchat.xhpolaris.com/comment/get_comments");
+            editor_CommunityFragment.putString("获取一些动态预览", "https://meowchat.xhpolaris.com/moment/get_moment_previews");
+            editor_CommunityFragment.putString("获取点赞数量", "https://meowchat.xhpolaris.com/like/get_count");
+            editor_CommunityFragment.putString("获取评论", "https://meowchat.xhpolaris.com/comment/get_comments");
             editor_CommunityFragment.putString("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6MCwiZGV2aWNlSWQiOiIiLCJleHAiOjE2OTY4MTkyMDAsImlhdCI6MTY5NjczMjgwMCwidXNlcklkIjoiNjQwYzMwMGU3NWJlOTYzNzNlMmU2ODZiIiwid2VjaGF0VXNlck1ldGEiOnsiYXBwSWQiOiJ3eGQzOWNlYmYwNWUyMWQzYjYiLCJvcGVuSWQiOiJvclgwRzQ2WFh2a0dzQ2FFbld3NjRBVDNQOUM0IiwidW5pb25JZCI6Im9WNmtVNTBFbUppR2tHTUFmS2YtRHMxa292TUUifX0.O8P-os_r6_sWonW7mHCRzvUXte44_Hx9-IV_5AJoqko");
             editor_CommunityFragment.putString("school_now", "华东师范大学中山北路校区");
             editor_CommunityFragment.putString("华东师范大学中山北路校区", "637ce159b15d9764c31f9c84");
@@ -153,7 +153,8 @@ public class CommunityFragment extends Fragment{
 
         return root;
     }
-    private void okHttp_init_moment(){
+
+    private void okHttp_init_moment() {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .retryOnConnectionFailure(true)
                 .connectTimeout(8000, TimeUnit.SECONDS)
@@ -161,7 +162,7 @@ public class CommunityFragment extends Fragment{
                 .writeTimeout(8000, TimeUnit.SECONDS)
                 .build();
         LinkedHashMap<String, String> params = new LinkedHashMap<>();
-        params.put("page",page.toString());
+        params.put("page", page.toString());
         params.put("communityId",
                 getter_CommunityFragment.getString(getter_CommunityFragment.getString("school_now",
                                 "华东师范大学中山北路校区"),
@@ -183,6 +184,7 @@ public class CommunityFragment extends Fragment{
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 System.out.println(call + "失败");
             }
+
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 if (response.isSuccessful()) {
@@ -191,20 +193,21 @@ public class CommunityFragment extends Fragment{
                     List<Moment> moments = momentResult.getMoments();
                     List<like_get_count_result> likeGetCountResults = new ArrayList<>();
                     ArrayList<get_comments_result> getCommentsResults = new ArrayList<>();
-                    for(Moment moment : moments){
+                    for (Moment moment : moments) {
                         likeGetCountResults.add(okhttp_get_moment_like_num(moment, "4"));
-                        getCommentsResults.add(okhttp_get_comments(moment,"3","0"));
+                        getCommentsResults.add(okhttp_get_comments(moment, "3", "0"));
                     }
-                    MomentAllDetail momentAllDetail = new MomentAllDetail(momentResult,likeGetCountResults,getCommentsResults);
+                    MomentAllDetail momentAllDetail = new MomentAllDetail(momentResult, likeGetCountResults, getCommentsResults);
                     Message msg = new Message();
-                    msg.what=2;
+                    msg.what = 2;
                     msg.obj = momentAllDetail;
                     handler.sendMessage(msg);
                 }
             }
         });
     }
-    private like_get_count_result okhttp_get_moment_like_num(Moment moment,String targetType) throws IOException {
+
+    private like_get_count_result okhttp_get_moment_like_num(Moment moment, String targetType) throws IOException {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .retryOnConnectionFailure(true)
                 .connectTimeout(8000, TimeUnit.SECONDS)
@@ -212,8 +215,8 @@ public class CommunityFragment extends Fragment{
                 .writeTimeout(8000, TimeUnit.SECONDS)
                 .build();
         LinkedHashMap<String, String> params = new LinkedHashMap<>();
-        params.put("targetId",moment.getId());
-        params.put("targetType",targetType);
+        params.put("targetId", moment.getId());
+        params.put("targetType", targetType);
 
         String url = my_utils.concat_url_params(getter_CommunityFragment.getString("获取点赞数量", ""), params);
         Request request = new Request.Builder()
@@ -231,7 +234,7 @@ public class CommunityFragment extends Fragment{
         return null;
     }
 
-    private get_comments_result okhttp_get_comments(Moment moment,String type,String page) throws IOException {
+    private get_comments_result okhttp_get_comments(Moment moment, String type, String page) throws IOException {
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .retryOnConnectionFailure(true)
                 .connectTimeout(8000, TimeUnit.SECONDS)
@@ -239,9 +242,9 @@ public class CommunityFragment extends Fragment{
                 .writeTimeout(8000, TimeUnit.SECONDS)
                 .build();
         LinkedHashMap<String, String> params = new LinkedHashMap<>();
-        params.put("id",moment.getId());
-        params.put("type",type);
-        params.put("page",page);
+        params.put("id", moment.getId());
+        params.put("type", type);
+        params.put("page", page);
 
         String url = my_utils.concat_url_params(getter_CommunityFragment.getString("获取评论", ""), params);
         Request request = new Request.Builder()
@@ -289,13 +292,14 @@ public class CommunityFragment extends Fragment{
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 System.out.println(call + "失败");
             }
+
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 if (response.isSuccessful()) {
                     assert response.body() != null;
                     news_result newsResult = new Gson().fromJson(response.body().string(), news_result.class);
                     Message msg = new Message();
-                    msg.what=1;
+                    msg.what = 1;
                     msg.obj = newsResult;
                     handler.sendMessage(msg);
                 }
